1f8b04adaf04bb66b0e3ed929f622d8e3c46de99
Shubham <shubhamb1810@gmail.com>
2019-11-23 16:49:17 -0500

    added year file

---
+++ src/main/java/Year.java
@@ -0,0 +1,74 @@
+public class Year {
+    private int[] zeroToNineteen;
+    private int[] zeroToFour;
+    private int[] fiveToNine;
+    private int[] tenToFourteen;
+    private int[] fifteenToNineteen;
+    private int[] twentyToFortyfour;
+    private int[] fortyfiveToSixtyfour;
+    private int[] sixtyFivePlus;
+
+    public int[] getZeroToNineteen() {
+        return zeroToNineteen;
+    }
+
+    public void setZeroToNineteen(int[] zeroToNineteen) {
+        this.zeroToNineteen = zeroToNineteen;
+    }
+
+    public int[] getZeroToFour() {
+        return zeroToFour;
+    }
+
+    public void setZeroToFour(int[] zeroToFour) {
+        this.zeroToFour = zeroToFour;
+    }
+
+    public int[] getFiveToNine() {
+        return fiveToNine;
+    }
+
+    public void setFiveToNine(int[] fiveToNine) {
+        this.fiveToNine = fiveToNine;
+    }
+
+    public int[] getTenToFourteen() {
+        return tenToFourteen;
+    }
+
+    public void setTenToFourteen(int[] tenToFourteen) {
+        this.tenToFourteen = tenToFourteen;
+    }
+
+    public int[] getFifteenToNineteen() {
+        return fifteenToNineteen;
+    }
+
+    public void setFifteenToNineteen(int[] fifteenToNineteen) {
+        this.fifteenToNineteen = fifteenToNineteen;
+    }
+
+    public int[] getTwentyToFortyfour() {
+        return twentyToFortyfour;
+    }
+
+    public void setTwentyToFortyfour(int[] twentyToFortyfour) {
+        this.twentyToFortyfour = twentyToFortyfour;
+    }
+
+    public int[] getFortyfiveToSixtyfour() {
+        return fortyfiveToSixtyfour;
+    }
+
+    public void setFortyfiveToSixtyfour(int[] fortyfiveToSixtyfour) {
+        this.fortyfiveToSixtyfour = fortyfiveToSixtyfour;
+    }
+
+    public int[] getSixtyFivePlus() {
+        return sixtyFivePlus;
+    }
+
+    public void setSixtyFivePlus(int[] sixtyFivePlus) {
+        this.sixtyFivePlus = sixtyFivePlus;
+    }
+}
